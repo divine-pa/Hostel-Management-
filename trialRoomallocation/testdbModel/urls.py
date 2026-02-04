1c73d14cb01b5cbcbbdc14c229b25c50131a8680
xprogram24 <cexcel58@gmail.com>
2026-02-05 00:50:14 +0100

    feat: Initialize React frontend with routing and authentication, and Django REST backend with API structure and documentation.
    tested  display of student data on the student dashboard.
    
    corrected error with admin / student login test

--- trialRoomallocation/testdbModel/urls.py
+++ trialRoomallocation/testdbModel/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_student,get_admin,get_hall,get_payment,student_login,admin_login
+from .views import get_student,get_admin,get_hall,get_payment,student_login,admin_login,student_dashboard
 
 urlpatterns = [
     path('student/', get_student), #path for student data 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('payment/', get_payment),
     path('student/login/', student_login),
     path('admin/login/', admin_login),
+    path('student/dashboard/', student_dashboard),
 ]  
